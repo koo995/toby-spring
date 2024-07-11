9cf154bc6951396db34dd1a5d4d1c78d591b4f40
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-12 03:00:55 +0900

    관계설정 책임의 분리 -client 로 책임을 넘겼다.
    하지만 클리이언트는 두가지 관심사를 가지고 있는 문제가 있다.

--- src/main/java/spring/toby/Client.java
+++ src/main/java/spring/toby/Client.java
@@ -2,9 +2,13 @@ package spring.toby;
 
 import java.io.IOException;
 
+/**
+ * 관계 설정의 책임을 Client 가 가져오게 되었다.
+ * 필요하다면 SimpleExRateProvider 를 WebApiExRateProvider 로 바꾸기만 하면 된다.
+ */
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService() ;
+        PaymentService paymentService = new PaymentService(new WebApiExRateProvider()) ;
         Payment payment = paymentService.prepare(100L, "USD", java.math.BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
