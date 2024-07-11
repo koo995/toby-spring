da2985d12c14d5e9db55e5f869b794fa830e11ea
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-11 22:46:02 +0900

    주석 제거

--- src/main/java/spring/toby/PaymentService.java
+++ src/main/java/spring/toby/PaymentService.java
@@ -13,24 +13,11 @@ import java.util.stream.Collectors;
 
 public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        /**
-         * 환율 가져오기 by httpie
-         * http -v https://open.er-api.com/v6/latest/USD
-         * 이용하면 터미널에서 가능
-         */
+
+        // 환율 가져오기 by httpie
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        /**
-         * inputStream 으로 받은 리턴 값은 사람이 알아볼 수 없다.
-         * java 문자로 변경하기위해 InputStreamReader를 사용한다.
-         * 사람이 알아볼 수 있게 하기 위해 BufferedReader를 사용한다.
-         * 이런 것은 실무에서 꽤 자주 볼 수 있다.
-         */
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        /**
-         * java8 이후로 추가된 메소드
-         * lines() 메소드는 BufferedReader의 모든 라인을 Stream으로 반환한다.
-         */
         String response = br.lines().collect(Collectors.joining());
         br.close();
 
