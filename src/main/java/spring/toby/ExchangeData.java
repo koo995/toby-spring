0a4cf1dee7d968883d2d0c938b1cc40de6765922
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-11 18:45:55 +0900

    record 클래스와 @JsonIgnoreProperties

---
+++ src/main/java/spring/toby/ExchangeData.java
@@ -0,0 +1,14 @@
+package spring.toby;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+/**
+ * 레코드의 주의할 점은 한번 값을 넘은 다음 수정할 수 없다.
+ * 우리는 json 에서 필요한 것만 뽑아서 사용할 것이니, 생성자에 없는 것들은 무시하자
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExchangeData(String result, Map<String, BigDecimal> rates) {
+}
