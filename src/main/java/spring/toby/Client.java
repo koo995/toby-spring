8dba95805636135e60c0814ce9672cc4d18c2c07
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-12 19:28:10 +0900

    DI와 디자인 패턴 - 데코레이터 패턴
    환율 정보가 필요할 때 매번 web api를 호출해야 할까?
    환율 정보가 필요한 기능 증가
    응답 시간
    환율 변동 주기
    여러가지 고려사항이 생기면... 매번 api 호출하기는 어려워진다.
    -> 환율 정보 캐시의 도입
    데코레이터 패턴을 응용하면 캐시가 가능하다!

--- src/main/java/spring/toby/Client.java
+++ src/main/java/spring/toby/Client.java
@@ -4,21 +4,32 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 관계 설정의 책임을 Client 가 가져오게 되었다.
  * 필요하다면 SimpleExRateProvider 를 WebApiExRateProvider 로 바꾸기만 하면 된다.
  */
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         /**
          * 이 빈팩토리는 스프링에 있는 빈 팩토리다. 우리 만든 애플리케이션의 구성정보를 모른다.
          * 그 구성정보를 참고하도록 해줘야한다.
          */
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
-        Payment payment = paymentService.prepare(100L, "USD", java.math.BigDecimal.valueOf(50.7));
-        System.out.println(payment);
+        Payment payment1 = paymentService.prepare(100L, "USD", java.math.BigDecimal.valueOf(50.7));
+        System.out.println(payment1);
+        Payment payment2 = paymentService.prepare(100L, "USD", java.math.BigDecimal.valueOf(50.7));
+        System.out.println(payment2);
+
+        TimeUnit.SECONDS.sleep(5);
+        Payment payment3 = paymentService.prepare(100L, "USD", java.math.BigDecimal.valueOf(50.7));
+        System.out.println(payment3);
+
+
+
+
     }
 }
 
--- src/main/java/spring/toby/ObjectFactory.java
+++ src/main/java/spring/toby/ObjectFactory.java
@@ -1,24 +1,29 @@
 package spring.toby;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+//@ComponentScan
 public class ObjectFactory {
 
-//    @Bean
-//    public PaymentService paymentService() {
-//        return new PaymentService(exRateProvider()) ;
-//    }
-//
-//    /**
-//     * 이렇게 분리하면 뭐가 좋냐?
-//     * 나중에 ExRateProvider 를 바꾸고 싶다면
-//     * 메서드를 통해 빨리 찾아갈 수 있다.
-//     */
-//    @Bean
-//    public ExRateProvider exRateProvider() {
-//        return new WebApiExRateProvider();
-//    }
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(cachedExRateProvider()) ;
+    }
+
+    @Bean
+    public ExRateProvider cachedExRateProvider() {
+        return new CachedExRateProvider(exRateProvider());
+    }
+
+    /**
+     * 이렇게 분리하면 뭐가 좋냐?
+     * 나중에 ExRateProvider 를 바꾸고 싶다면
+     * 메서드를 통해 빨리 찾아갈 수 있다.
+     */
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
 }
