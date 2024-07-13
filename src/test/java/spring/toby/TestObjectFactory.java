fbf31aa3e8dac58e3bcbfacff39a2fd28394d96e
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-13 19:11:39 +0900

    테스트와 DI(2) - 스프링 DI를 이용하는 테스트

---
+++ src/test/java/spring/toby/TestObjectFactory.java
@@ -0,0 +1,24 @@
+package spring.toby;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.toby.payment.ExRateProvider;
+import spring.toby.payment.ExRateProviderStub;
+import spring.toby.payment.PaymentService;
+
+import java.math.BigDecimal;
+
+@Configuration
+//@ComponentScan
+public class TestObjectFactory {
+
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider()) ;
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new ExRateProviderStub(BigDecimal.valueOf(1_000));
+    }
+}
--- src/test/java/spring/toby/payment/PaymentServiceSpringTest.java
+++ src/test/java/spring/toby/payment/PaymentServiceSpringTest.java
@@ -1,7 +1,10 @@
-package spring.toby.payment;
+ package spring.toby.payment;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.toby.TestObjectFactory;
 
 import java.time.LocalDateTime;
 
@@ -9,26 +12,23 @@ import static java.math.BigDecimal.TEN;
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class PaymentServiceTest {
+class PaymentServiceSpringTest {
 
     @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
     @Test
     void prepare() throws Exception {
         // given
-        PaymentService paymentService = new PaymentService(new ExRateProviderStub(valueOf(500)));
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         // when
         Payment payment = paymentService.prepare(100L, "USD", TEN);
 
         // then
-        /**
-         * 환율정보 가져온다.
-         * BigDecimal 을 이용한 비교를 할땐 isEqualByComparingTo 를 사용하자.
-         */
-        assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(500));
+        assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(1_000));
 
         // 완화환산금액 계산
-        assertThat(payment.getConvertedAmount()).isEqualTo(valueOf(5_000));
+        assertThat(payment.getConvertedAmount()).isEqualTo(valueOf(10_000));
 
         // 완화환산금액의 유효시간 계산
         // 이것도 검증하기가 만만치 않다..
