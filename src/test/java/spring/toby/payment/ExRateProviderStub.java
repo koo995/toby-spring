cb94a126dd75200ed04ec96006237425657207ab
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-13 18:45:25 +0900

    테스트와 DI(1) - 외부 api 객체에 해당하는 stub 사용

---
+++ src/test/java/spring/toby/payment/ExRateProviderStub.java
@@ -0,0 +1,30 @@
+package spring.toby.payment;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+/**
+ * 무엇을 리턴해줄까?
+ * 중요한 것은 테스트 코드에서 얘가 리턴하는 값이 뭐다 라는 것 미리 알고있는 채로 사용할 수 있게 해주는 것
+ */
+public class ExRateProviderStub implements ExRateProvider {
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public void setExRate(BigDecimal exRate) {
+        this.exRate = exRate;
+    }
+
+    private BigDecimal exRate;
+
+    public ExRateProviderStub(BigDecimal exRate) {
+        this.exRate = exRate;
+    }
+
+    @Override
+    public BigDecimal getExRate(String currency) throws IOException {
+        return exRate;
+    }
+}
--- src/test/java/spring/toby/payment/PaymentServiceTest.java
+++ src/test/java/spring/toby/payment/PaymentServiceTest.java
@@ -2,11 +2,11 @@ package spring.toby.payment;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import spring.toby.exrate.WebApiExRateProvider;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static java.math.BigDecimal.TEN;
+import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PaymentServiceTest {
@@ -15,19 +15,20 @@ class PaymentServiceTest {
     @Test
     void prepare() throws Exception {
         // given
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(valueOf(500)));
 
         // when
-        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.TEN);
+        Payment payment = paymentService.prepare(100L, "USD", TEN);
 
         // then
-        // 환율정보 가져온다. 다만 정확한 값을 알 방법이 없다. isEqualTo 을 쓰긴 어렵다.
-        // 가장 쉬운 방법은... 있는지 없는지?
-        assertThat(payment.getExRate()).isNotNull();
+        /**
+         * 환율정보 가져온다.
+         * BigDecimal 을 이용한 비교를 할땐 isEqualByComparingTo 를 사용하자.
+         */
+        assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(500));
 
         // 완화환산금액 계산
-        assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
+        assertThat(payment.getConvertedAmount()).isEqualTo(valueOf(5_000));
 
         // 완화환산금액의 유효시간 계산
         // 이것도 검증하기가 만만치 않다..
