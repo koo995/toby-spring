69c7343962cbd365eb744f8c0ef659b048b025ab
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-16 15:30:43 +0900

    ApiExecutor 콜백과 메소드 주입

--- src/main/java/spring/toby/exrate/WebApiExRateProvider.java
+++ src/main/java/spring/toby/exrate/WebApiExRateProvider.java
@@ -2,6 +2,7 @@ package spring.toby.exrate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import spring.toby.api.ApiExecutor;
 import spring.toby.api.SimpleApiExecutor;
 import spring.toby.payment.ExRateProvider;
 
@@ -13,21 +14,22 @@ import java.net.URISyntaxException;
 public class WebApiExRateProvider implements ExRateProvider {
 
     /**
-     * @throws IOException 은 우리가 제어할 수 없는 외부의 문제이다. 인터넷을 끊어졌거나... api 제공 업체에 장애가 나타났거나..
-     * 진짜 예외적인 상황이다.
-     * 캐치를 해가지고 무언가를 처리하지 않으면 안되는 상황.
-     * 반드시 예외적인 상황이다라는 것을 시스템의 앞단으로 던져줘야 한다.
-     * runtime exception 을 던지면 된다.
-     * 예외를 캐치해가지고 뭔가를 처리하는 이유는 복구하기 위함.
-     * 그러나 그 상황을 복구할만한 설계가 없다면 무시하면 된다.
+     * 템플릿의 메서드를 호출해주는 이 코드가 클라이언트가 된다.
      */
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
-        return runApiForExRate(url);
+        return runApiForExRate(url, new SimpleApiExecutor());
     }
 
-    private static BigDecimal runApiForExRate(String url) {
+    /**
+     * 인터페이스 타입으로 정의한다.
+     * 실행해야하는 시점에 인터페이스 타입에 어떤 클래스의 오브젝트인지 몰라야한다.
+     * 그래서 어디서 주입하냐?
+     * 우리가 작성하는 코드에 의해서. SimpleApiExecutor 이 바로 콜백이다. 변경이 필요할 때, 이 클래스를 변경하면 된다. 클래스를 정의하지 않고 람다를 사용해도 된다.
+     * 이 콜백을 받아서 내부에서 수행하는 runApiForExRate 이 템플릿이다.
+     */
+    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor) {
         URI uri;
         try {
             uri = new URI(url);
@@ -37,7 +39,7 @@ public class WebApiExRateProvider implements ExRateProvider {
 
         String response;
         try {
-            response = new SimpleApiExecutor().execute(uri);
+            response = apiExecutor.execute(uri);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
