3b95f0786c2437a166fe92fdbeeab472dde399f0
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-16 16:17:59 +0900

    ApiTemplate 분리
    참고로 콜백을 클라이언트에서 직접 정의해 가지고 만들었다면 클라이언트가 가지고 있는 final 정보를 참조할 수 있다. 무슨 장점이냐? 콜백에 생성자로 정보를 넣어줄 필요가 없다.

--- src/main/java/spring/toby/exrate/WebApiExRateProvider.java
+++ src/main/java/spring/toby/exrate/WebApiExRateProvider.java
@@ -1,54 +1,24 @@
 package spring.toby.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import spring.toby.api.ApiExecutor;
+import spring.toby.api.ApiTemplate;
 import spring.toby.api.ErApiExRateExtractor;
-import spring.toby.api.ExRateExtractor;
 import spring.toby.api.SimpleApiExecutor;
 import spring.toby.payment.ExRateProvider;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class WebApiExRateProvider implements ExRateProvider {
 
     /**
-     * 템플릿의 메서드를 호출해주는 이 코드가 클라이언트가 된다.
+     * 템플릿을 만들어서 분리하였다.
+     * 템플릿은 그 안에 상태값을 가지고 계속 변경되는 것이 아니고 서버에서 동작한다고 한다 하더라도
+     * 멀티스레드에서 안전하므로 클래스의 인스턴스가 만들어질때 딱 하나만 만들어지도록하고 재사용하도록한다.
      */
+    ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
-        return runApiForExRate(url, new SimpleApiExecutor(),new ErApiExRateExtractor());
-    }
-
-    /**
-     * 인터페이스 타입으로 정의한다.
-     * 실행해야하는 시점에 인터페이스 타입에 어떤 클래스의 오브젝트인지 몰라야한다.
-     * 그래서 어디서 주입하냐?
-     * 우리가 작성하는 코드에 의해서. SimpleApiExecutor 이 바로 콜백이다. 변경이 필요할 때, 이 클래스를 변경하면 된다. 클래스를 정의하지 않고 람다를 사용해도 된다.
-     * 이 콜백을 받아서 내부에서 수행하는 runApiForExRate 이 템플릿이다.
-     */
-    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExecutor.execute(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return exRateExtractor.extract(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return apiTemplate.getExRate(url, new SimpleApiExecutor(),new ErApiExRateExtractor());
     }
 }
