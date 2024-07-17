601be9ed2e7ed233c2f082213ccbc3d22d962825
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-18 03:56:24 +0900

    Order 클래스에서 jpa 정보 제거

--- src/main/java/spring/toby/order/Order.java
+++ src/main/java/spring/toby/order/Order.java
@@ -1,17 +1,11 @@
 package spring.toby.order;
 
-import jakarta.persistence.*;
-
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
 public class Order {
 
-    @Id @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
     private String no;
 
     private BigDecimal total;
@@ -35,6 +29,4 @@ public class Order {
     public BigDecimal getTotal() {
         return total;
     }
-
-
 }
