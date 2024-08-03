22d42b8387de0e0c952446297e30bb0fe25c40ae
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-04 02:25:33 +0900

    초난감 dao 초기 세팅

--- src/main/java/spring/toby/TobyApplication.java
+++ src/main/java/spring/toby/TobyApplication.java
@@ -2,12 +2,33 @@ package spring.toby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import spring.toby.user.User;
+import spring.toby.user.UserDao;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class TobyApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         SpringApplication.run(TobyApplication.class, args);
+
+        UserDao dao = new UserDao();
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+
     }
 
 }
---
+++ src/main/java/spring/toby/user/User.java
@@ -0,0 +1,12 @@
+package spring.toby.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    String id;
+    String name;
+    String password;
+}
