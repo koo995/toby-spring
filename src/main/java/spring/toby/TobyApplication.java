abbc9bbd991924de6d9eed35c0849e3c2e36bd85
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-04 12:44:24 +0900

    관계설정책임의 분리 - 전략패턴 도입

--- src/main/java/spring/toby/TobyApplication.java
+++ src/main/java/spring/toby/TobyApplication.java
@@ -2,6 +2,8 @@ package spring.toby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import spring.toby.user.ConnectionMaker;
+import spring.toby.user.DConnectionMaker;
 import spring.toby.user.User;
 import spring.toby.user.UserDao;
 
@@ -12,8 +14,9 @@ public class TobyApplication {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         SpringApplication.run(TobyApplication.class, args);
+        ConnectionMaker connectionMaker = new DConnectionMaker();
 
-        UserDao dao = new UserDao();
+        UserDao dao = new UserDao(connectionMaker);
         User user = new User();
         user.setId("whiteship");
         user.setName("백기선");
