85045a32835c6af65be7c379df7f5698b8da6296
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-05 23:39:28 +0900

    불필요한 클래스 제거

--- src/main/java/spring/toby/user/ConnectionMaker.java
+++
@@ -1,7 +0,0 @@
-package spring.toby.user;
-
-import java.sql.Connection;
-
-public interface ConnectionMaker {
-    Connection makeConnection() throws ClassNotFoundException, java.sql.SQLException;
-}
