a0125fa8b1ac6125eaa362834a9fcc9b0252ac60
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-06 01:50:41 +0900

    콜백과 템플릿의 결합 - 콜백 재활용

--- src/main/java/spring/toby/user/UserDao.java
+++ src/main/java/spring/toby/user/UserDao.java
@@ -56,12 +56,7 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                return c.prepareStatement("delete from users");
-            }
-        });
+        this.jdbcContext.executeSql("delete from users");
     }
 
     public int getCount() throws SQLException {
