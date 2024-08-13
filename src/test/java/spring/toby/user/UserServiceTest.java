31f546fdd9346d957934038f9e23a2ceed199b15
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-13 11:42:04 +0900

    MockUserDao 를 사용해서 만든 고립된 테스트

--- src/test/java/spring/toby/user/UserServiceTest.java
+++ src/test/java/spring/toby/user/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -44,19 +45,22 @@ class UserServiceTest {
     @DisplayName("업그레이드 레벨 테스트")
     @Test
     void upgradeLevels() throws Exception {
+        MockUserDao mockUserDao = new MockUserDao(users);
+        UserServiceImpl userServiceImpl = new UserServiceImpl(mockUserDao); // 고립된 테스트에서는 테스트 대상 오브젝트를 직접 생성하면 된다.
+
+
         // given
         userDao.deleteAll();
         for (User user : users) {
             userDao.add(user);
         }
 
-        userService.upgradeLevels();
+        userServiceImpl.upgradeLevels(); // db 에 저장만 안되었지 업그레이드가 되었는지 안되었는지는 확인이 되구나
 
-        checkLevelUpgraded(users.get(0), false);
-        checkLevelUpgraded(users.get(1), true);
-        checkLevelUpgraded(users.get(2), false);
-        checkLevelUpgraded(users.get(3), true);
-        checkLevelUpgraded(users.get(4), false);
+        List<User> updated = mockUserDao.getUpdated();
+        assertThat(updated.size()).isEqualTo(2);
+        checkUserAndLevel(updated.get(0), "joytouch", Level.SILVER);
+        checkUserAndLevel(updated.get(1), "madnite1", Level.GOLD);
     }
 
     @DisplayName("사용자 추가 테스트")
@@ -97,6 +101,11 @@ class UserServiceTest {
         checkLevelUpgraded(users.get(1), false); // 즉 업그레이드 되었단 말이지
     }
 
+    private void checkUserAndLevel(User updatedUser, String expectedId, Level expectedLevel) {
+        assertThat(updatedUser.getId()).isEqualTo(expectedId);
+        assertThat(updatedUser.getLevel()).isEqualTo(expectedLevel);
+    }
+
     private void checkLevelUpgraded(User user, boolean upgraded) {
         User userUpdate = userDao.get(user.getId());
         if (upgraded) {
@@ -105,6 +114,7 @@ class UserServiceTest {
             assertThat(userUpdate.getLevel()).isEqualTo(user.getLevel());
         }
     }
+
     static class TestUserService extends UserServiceImpl {
         private String id;
 
@@ -121,5 +131,48 @@ class UserServiceTest {
     }
 
     static class TestUserServiceException extends RuntimeException {
+    }
+
+    static class MockUserDao implements UserDao {
+        private List<User> users;
+        private List<User> updated = new ArrayList<>();
+
+        private MockUserDao(List<User> users) {
+            this.users = users;
+        }
+
+        public List<User> getUpdated() {
+            return this.updated;
+        }
+
+        @Override
+        public List<User> getAll() {
+            return this.users;
+        }
+
+        @Override
+        public void update(User user) {
+            updated.add(user);
+        }
+
+        @Override
+        public void add(User user) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void deleteAll() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public User get(String id) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int getCount() {
+            throw new UnsupportedOperationException();
+        }
     }
 }
